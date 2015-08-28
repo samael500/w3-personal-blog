b57603d998a53aec40274a2f737dd1031ca8f5c3
Samael500 <samael500@gmail.com>
2015-08-28 15:48:13 +0300

    :rocket: add theme template

---
+++ static/js/tree.js
@@ -0,0 +1,58 @@
+$( document ).ready(function() {
+$.fn.extend({
+    treed: function (o) {
+      
+      var openedClass = 'glyphicon-minus-sign';
+      var closedClass = 'glyphicon-plus-sign';
+      
+      if (typeof o != 'undefined'){
+        if (typeof o.openedClass != 'undefined'){
+        openedClass = o.openedClass;
+        }
+        if (typeof o.closedClass != 'undefined'){
+        closedClass = o.closedClass;
+        }
+      };
+      
+        //initialize each of the top levels
+        var tree = $(this);
+        tree.addClass("tree");
+        tree.find('li').has("ul").each(function () {
+            var branch = $(this); //li with children ul
+            branch.prepend("<i class='indicator glyphicon " + openedClass + "'></i> ");
+            branch.addClass('branch');
+            branch.find('i.indicator.glyphicon').on('click', function (e) {
+                var $li_branch = $(this).parent();
+                if (this == e.target) {
+                    var icon = $li_branch.children('i:first');
+                    icon.toggleClass(openedClass + " " + closedClass);
+                    $li_branch.children().children().toggle();
+                }
+            })
+            // branch.children().children().toggle();
+        });
+        //fire event from the dynamically added icon
+      tree.find('.branch .indicator').each(function(){
+        $(this).on('click', function () {
+            $(this).closest('li').click();
+        });
+      });
+        //fire event to open branch if the li contains an anchor instead of text
+        tree.find('.branch>a').each(function () {
+            $(this).on('click', function (e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+        //fire event to open branch if the li contains a button instead of text
+        tree.find('.branch>button').each(function () {
+            $(this).on('click', function (e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+    }
+});
+
+$('#tree').treed({openedClass:'glyphicon-folder-open', closedClass:'glyphicon-folder-close'});
+});
